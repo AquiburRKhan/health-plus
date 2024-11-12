69ca540a01324ad49dd2d33e97cf3d11099894f9
AquiburRahmanKhan <aquibur.rahman@monstar-lab.com>
2024-11-12 16:52:08 +0600

    feat: Home page for Dr Mahmuda Dewan

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,115 @@
 import Image from "next/image";
 
+const services = [
+  "Diagnosing and Treating Illnesses",
+  "Prescribing Medication",
+  "Health Screenings and Preventive Care",
+  "Patient Education",
+  "Coordinating Referrals",
+  "Managing Chronic Conditions",
+  "Conducting Physical Examinations",
+  "Initial X-Ray Interpretation & Follow-up care",
+];
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <>
+      {/* Header */}
+      <header className="flex justify-between pt-4 md:py-4 px-6 bg-blue-500">
+        <div className="flex gap-3 items-center">
+          <div className="h-12 w-12">
             <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/logo.jpeg"
+              alt="Doctor Logo"
+              className="rounded-md"
+              width={100}
+              height={100}
+              priority
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <span className="text-2xl font-roboto font-bold text-white">
+            HEALTH PLUS
+          </span>
         </div>
+      </header>
+
+      {/* Main */}
+      <main>
+        <section className="flex justify-center py-10 md:py-24 px-4 sm:px-0 bg-gradient-to-t from-[#5f16ea] to-blue-500">
+          <div className="container flex flex-col gap-4 md:gap-8 text-center">
+            <h1 className="text-xl font-roboto font-bold text-white">
+              WELCOME TO HEALTH PLUS
+            </h1>
+            <h2 className="text-5xl font-roboto font-bold text-white">
+              DOCTOR&apos;S HUB <br className="md:hidden" /> &{" "}
+              <br className="md:hidden" /> PHARMACY
+            </h2>
+            <div className="flex justify-center">
+              <a
+                href="tel:+8801324404552"
+                className="text-xl font-inter text-white py-3 px-8 border border-white rounded-md"
+              >
+                +8801324404552
+              </a>
+            </div>
+            <div className="text-base font-inter text-white">
+              House: 408/3, Road: 07 (West),
+              <br className="sm:hidden" /> DOHS Baridhara,
+              <br />
+              Dhaka Cantonment, Dhaka - 1206
+            </div>
+          </div>
+        </section>
+
+        <section className="flex justify-center py-10 md:py-24 px-4 sm:px-0">
+          <div className="container flex flex-col md:flex-row gap-4 md:gap-24 w-fit items-center">
+            <div className="flex justify-end w-60 h-60">
+              <Image
+                src="/doctor.jpeg"
+                alt="Doctor Image"
+                className="rounded-md shadow-[0_0_5px_rgba(0,0,0,0.8)]"
+                width={240}
+                height={240}
+                priority
+              />
+            </div>
+            <div className="flex flex-col text-center md:text-left gap-2 sm:gap-4">
+              <h3 className="text-4xl font-roboto font-semibold">
+                DR MAHMUDA DEWAN
+              </h3>
+              <div className="flex flex-col gap-1">
+                <p className="text-lg font-inter">GENERAL PRACTITIONER</p>
+                <p className="text-lg font-inter">MBBS (DU), CCD (BIRDEM)</p>
+                <p className="text-lg font-inter">
+                  TRAINING ON MEDICINE & DIABETOLOGY
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="flex justify-center py-10 gap-8 md:py-24 px-4 sm:px-0 bg-gradient-to-b from-[#5f16ea] to-blue-500 text-center">
+          <div className="container flex flex-col gap-4 md:gap-8">
+            <h3 className="text-4xl font-roboto font-bold text-white">
+              TYPES OF CARE
+            </h3>
+            <div className="container grid grid-cols-2 gap-4 md:gap-8">
+              {services.map((service, index) => (
+                <div
+                  key={index}
+                  className="flex items-center justify-center rounded-md border-2 font-inter uppercase border-white p-3 text-white"
+                >
+                  {service}
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+        <section className="flex justify-center py-2 md:py-4 bg-black">
+          <p className="text-sm font-roboto font-semibold text-white">
+            Made By Aquibur Rahman Khan
+          </p>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
